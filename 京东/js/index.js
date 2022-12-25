6950c139c3ebf28fdb442b1a5d939615c691d8fc
SokodomoLL <108043710+SokodomoLL@users.noreply.github.com>
2022-12-25 20:44:37 +0800

    Add files via upload

---
+++ 京东/js/index.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', function () {
+    var arrow_l = document.querySelector('.arrow-l');
+    var arrow_r = document.querySelector('.arrow-r');
+    var focus = document.querySelector('.focus');
+    var ul = focus.querySelector('ul');
+    var focusWidth = focus.offsetWidth;
+   
+    focus.addEventListener('mouseenter', function () {
+        arrow_l.style.display = 'block';
+        arrow_r.style.display = 'block';
+        clearInterval(timer);
+        timer = null; 
+    });
+    focus.addEventListener('mouseleave', function () {
+        arrow_l.style.display = 'none';
+        arrow_r.style.display = 'none';
+        timer = setInterval(function () {
+          
+            arrow_r.click();
+        }, 2000);
+    });
+   
+    var first = ul.children[0].cloneNode(true);
+    ul.appendChild(first);
+    var num = 0;
+    arrow_r.addEventListener('click', function () {
+        if (num == ul.children.length - 1) {
+            ul.style.left = 0;
+            num = 0;
+        }
+        num++;
+        animate(ul, -num * focusWidth, function () {
+            0
+        });
+    });
+    //左
+    arrow_l.addEventListener('click', function () {
+        if (num == 0) {
+                num = ul.children.length - 1;
+                ul.style.left = -num * focusWidth + 'px';
+            }
+            num--;
+            animate(ul, -num * focusWidth, function () {
+                flag = true;
+            });
+        });
+    })
